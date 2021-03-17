f1fc71d56d031da8933662c6a8b299c1218f6442
kalil cisse <kalilou.cisse.250@gmail.com>
2021-03-17 15:03:26 +0100

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='quote',
+setup(name='bbquote',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
@@ -13,5 +13,5 @@ setup(name='quote',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/quote-run'],
+      scripts=['scripts/bbquote-run'],
       zip_safe=False)
